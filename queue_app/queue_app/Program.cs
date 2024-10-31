db4d2add69c3d0d70b0e6bfe838747d06f072d7d
meakaliaG <124390595+meakaliaG@users.noreply.github.com>
2024-10-31 08:21:46 -0400

    Update Program.cs
    sample code

--- queue_app/queue_app/Program.cs
+++ queue_app/queue_app/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace queue_app
+﻿using System;
+using System.Collections.Generic;
+
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        // Print Queue
+        Queue<string> printQueue = new Queue<string>();
+
+        // Add documents to print queue
+        printQueue.Enqueue("Document1");
+        printQueue.Enqueue("Document2");
+        printQueue.Enqueue("Document3");
+
+        Console.WriteLine("What would you like to do?");
+        Console.WriteLine("Options:\n- 'Print' - Print documents in printer queue.\n-'Quit' - End program.");
+
+        Console.WriteLine("Print Queue:");
+        while (printQueue.Count > 0)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Printing: {printQueue.Dequeue()}");
         }
     }
 }
