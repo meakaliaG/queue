57edd0ca668480f2bf91fee092bf5ca45377be05
meakaliaG <124390595+meakaliaG@users.noreply.github.com>
2024-10-31 08:24:54 -0400

    stacks_and_queue

---
+++ stacks_and_queues_app/stacks_and_queues_app/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace stacks_and_queues_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
